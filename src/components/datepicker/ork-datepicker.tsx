2b8ef15502e5e3d31fba949677d7f0d440aaeaea
Andre Muehle <andre.muehle@migrosluzern.ch>
2018-09-12 10:24:26 +0200

    return array with days

--- src/components/datepicker/ork-datepicker.tsx
+++ src/components/datepicker/ork-datepicker.tsx
@@ -20,12 +20,10 @@ export class OrkDatepicker {
 
     openDialog() {
         this.dialogVisible = !this.dialogVisible;
-        console.log(this.dialogVisible);
         this.renderDialog();
     }
 
     renderDialog() {
-        console.log('renderDialog');
         let dialog = [];
         dialog.push(<input type="text" onClick={() => this.openDialog()}></input>)
 
@@ -94,34 +92,64 @@ export class OrkDatepicker {
     }
 
     createTableBody() {
-
         let map: string[][] = this.createMonthTable();
 
         let table = [];
 
-        for(var i = 0; i < map.length; i++) {
+        for (var i = 0; i < map.length; i++) {
             let line = map[i];
-            table.push('<tr>');
-            for(var j = 0; j < line.length; j++) {
-                table.push(<td>{map[i][j]}</td>)
-            }
-            table.push('</tr>');
+            table.push(<tr>{this.renderLine(line, map, i)}</tr>);
         }
 
         return table;
     }
 
+    renderLine(line: string[], map: string[][], i: number): any {
+        let tableLine = [];
+
+        for (var j = 0; j < line.length; j++) {
+            tableLine.push(<td>{map[i][j]}</td>)
+        }
+
+        return tableLine;
+    }
+
     createMonthTable(): any {
+
         let map: string[][] = new Array<Array<string>>();
+        let weekOfMonthIndex = 0;
+        map.push([]);
+
+        let emptyDays = new Date(this.date.getFullYear(), this.date.getMonth(), 1).getDay() - 1;
+        if (emptyDays == -1) emptyDays = 6;
+
+        for (let i = 1; i <= emptyDays; i++) {
+            map[weekOfMonthIndex].push('');
+        }
+
+        for (let i = 1; i <= this.getLastDayOfMonth(); i++) {
+            var month = this.date.getMonth();
+            var year = this.date.getFullYear();
+            var day = new Date(year, month, i);
+
+            map[weekOfMonthIndex].push('' + day.getDate());
+
+            if (day.getDay() == 0) {
+                weekOfMonthIndex++;
+                map.push([]);
+            }
+        }
 
         return map;
     }
 
     getLastDayOfMonth(): any {
-        var month = this.getCurrentMonth();
-        var day = new Date(2008, month + 1, 0);
 
-        return day
+        var month = this.date.getMonth();
+        var year = this.date.getFullYear();
+        var day = new Date(year, month + 1, 0);
+
+        return day.getDate();
     }
 
     render() {
